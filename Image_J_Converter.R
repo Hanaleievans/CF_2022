3f7fc36650e449d031f45ce98b503c86820b924d
Hanaleievans <hanaleievans@uri.edu>
2022-08-30 09:04:58 -0400

    new

--- Image_J_Converter.R
+++ Image_J_Converter.R
@@ -6,7 +6,7 @@ library(tidyverse)
 cheatsheet <- read.csv('~/Desktop/GitHub/CF_2022/CALICE.CHEATSHEET.csv')
 
 # Loading second sample
-ImageJSample <- read.csv('~/Desktop/GitHub/CF_2022/Image_J/OFAV_SP_2AZ7_P2.csv',na.strings=c("",".","NaN"))
+ImageJSample <- read.csv('~/Desktop/GitHub/CF_2022/Image_J/OFAV_PP_9AZ5.csv',na.strings=c("",".","NaN"))
 
 #Loading Master File
 master <- read.csv('~/Desktop/Github/CF_2022/Transplants_Calice_Master.csv')
@@ -17,8 +17,11 @@ ImageJSample$Label <- gsub(".jpg","",as.character(ImageJSample$Label))
 #Merging Samples 
 x <- merge(x= ImageJSample, y=cheatsheet, by= 0)
 
+x <- merge(x= ImageJSample, y=cheatsheet, by= 0) %>%
+  .[c(1,3:11)]
+
 #Write new csv 
-write.csv(x, '~/Desktop/GitHub/CF_2022/RawData/Merged_OFAV_SP_2AZ7_P2.csv')
+write.csv(x, '~/Desktop/GitHub/CF_2022/RawData/Merged_OFAV_PP_9AZ5.csv')
 
 
 #append the new data to the master
